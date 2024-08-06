1202b597c4f27b7125223954b6bb8592e383809c
JORGE MIKAEL COUTINHO ALVES <jmca.lic19@uea.edu.br>
2024-08-06 19:49:24 -0300

    ajustando metadada

--- dalygames/src/components/container/index.tsx
+++ dalygames/src/components/container/index.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from "react";
 
-export function Container({ children }: { children: ReactNode }) {
-    return (
-        <div className="max-w-screen-lg mx-auto px-3">
-            {children}
-        </div>
-    );
+export function Container({ children }: { children: ReactNode }){
+  return(
+    <div className="max-w-screen-xl mx-auto px-3">
+      {children}
+    </div>
+  )
 }
